7b9d7849891ebc14f76c41c0c8daeba07fe4cf4c
Junior Aguayo Cuadros <junior.aguayo.cuadros@gmail.com>
2019-07-30 20:27:21 -0500

    Configuracion espacios JSON

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const app = express();
 //settings
 app.set('port', process.env.PORT || 3000);
-
+app.set('json spaces', 2);
 
 //Middlewares
 app.use(morgan('dev')); 
